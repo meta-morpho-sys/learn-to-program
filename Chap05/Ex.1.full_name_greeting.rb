8f0d19554ad604739e9e0ede7295fdb922c7024e
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-06-17 16:37:15 +0100

    Fist version of ex.1 in Chap 5

---
+++ Chap05/Ex.1.full_name_greeting.rb
@@ -0,0 +1,9 @@
+=begin
+Type your name and this program will greet you.
+The "chomp method takes off any "enter" character"
+=end
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '.  ;-P'
