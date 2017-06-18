5d7b25ed120a96e77add745a53881b50184d185e
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-06-18 14:59:04 +0100

    Improved the code using the inline "if" statement

--- Chap09/Ex.1.ask_question_impr.rb
+++ Chap09/Ex.1.ask_question_impr.rb
@@ -2,12 +2,8 @@ def ask question
   while true
     puts question
     reply = gets.chomp.downcase
-    if reply == 'yes'
-      return true
-    end
-    if reply == 'no'
-      return false
-    end
+    return true if reply == 'yes'
+    return false if reply == 'no'
       puts 'Please answer "yes" or "no".'
   end
 end
