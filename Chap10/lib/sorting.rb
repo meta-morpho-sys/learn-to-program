b8dfdefba6f2aa45335f18b42ae5c6d808ec2fca
meta-morpho-sys <yuliya.nedyalkova777@gmail.com>
2017-07-02 21:12:25 +0100

    applied rspec to exercises in chapter 10.

--- Chap10/lib/sorting.rb
+++ Chap10/lib/sorting.rb
@@ -1,9 +1,19 @@
+def sort user_array
+  sort_helper user_array, []
+end
+
 def find_smallest arr
   smallest, index = arr.each_with_index.min
   arr.delete_at(index)
   [smallest, arr]
 end
 
-
-
-
+def sort_helper unsorted, sorted
+  if unsorted.empty?
+    sorted
+  else
+    smallest_found, remaining = find_smallest unsorted
+    sorted << smallest_found
+    sort_helper remaining, sorted
+  end
+end
--- Chap10/spec/sorting_spec.rb
+++ Chap10/spec/sorting_spec.rb
@@ -1,6 +1,8 @@
 require 'rspec'
 require 'sorting'
 
+
+
 describe 'find_smallest' do
 
   it 'finds the smallest item in an array' do
@@ -17,7 +19,64 @@ describe 'find_smallest' do
 
 end
 
+describe 'sort_helper' do
+
+  it 'exercises the helper that uses two arrays' do
+
+    sorted = [ ] # because there are no sorted items yet
+    unsorted = [5,3,6,4,8,1,6,3,8,7,7]  # initially all unsorted
+
+    result = sort_helper(unsorted, sorted)
+
+    expect(result.first).to eq 1
+    expect(result.last).to eq 8
+    # expect(result.length).to eq unsorted.length
+    expect(result.length).to eq 11
+
+  end
+
+  it 'gets the same result after repeating' do
+
+    sorted = [ ] # because there are no sorted items yet
+    unsorted = %w[This is also an awesome awesome thing!]  # initially all unsorted
+
+    result = sort_helper(unsorted, sorted)
+
+    sorted2 = [ ] # because there are no sorted items yet
+    unsorted2 = result.clone
+
+    result2 = sort_helper(unsorted2, sorted2)
+
+    expect(result).to eq result2
+  end
+
+  it 'sorts big shuffled arrays' do
+    sorted = []
+    arr = (1..1000).to_a
+    unsorted = arr.shuffle
+    result = sort_helper(unsorted, sorted)
+    expect(result).to eq arr
+  end
+
+  it 'tests that sort wrapper works' do
+    arr = (1..100).to_a
+    user_array = arr.reverse
+    result = sort user_array
+    expect(result).to eq arr
+  end
+
+  it 'tests that sort wrapper handles really big input' do
+    arr = (1..25_000).to_a
+    user_array = arr.reverse
+    result = sort user_array
+    expect(result).to eq arr
+  end
+
+end
 
 
-
+# it 'finds the smallest item in an array of strings (using a single result in the test)' do
+#   result = find_smallest [1, 2, 3]
+#   expect(result).to eq [1, [2, 3]]
+# end
 
